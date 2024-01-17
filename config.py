f4b5e2f9c9597f69d16e5ff4bcf00779a2d88543
reylass <39653647+reylass@users.noreply.github.com>
2024-01-17 19:35:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ AWAIT_RESPONSE_TIME = 7  # time (sec) from HLR request to API check status
 """Environment defines"""
 try:
     dotenv.load_dotenv('.env')
-    TOKEN = os.environ('TOKEN_BOT_HLRLOOKUP','6833163649:AAEG-3VVBOLIqogcPRFS-hU5B-a3NhQ5rJM')  # BotFather's token
-    API_KEY = os.environ.get['Tsetdua_bot']  # BSG World token
+    TOKEN = os.environ['Tsetdua_bot']  # BotFather's token
+    API_KEY = os.environ.get('TOKEN_API_HLRLOOKUP', 'test_DNWugcohqXPujEcURT1F')  # BSG World token
     ADMIN_CONTACT = os.environ.get('HLRLOOKUP_ADMIN_CONTACT')  # Telegram user link like @name
     ADMIN_ID = int(os.environ.get('HLRLOOKUP_ADMIN_ID'))  # Telegram user id like 12334542
     DB_PATH = os.path.abspath(os.environ.get('HLRLOOKUP_DB_FILE'))  # sqlite3 DB file
